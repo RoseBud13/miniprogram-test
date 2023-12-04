87ce7c805f1d95516acc586c9a70cc356d179d23
RoseBud13 <xiongkaijie13@gmail.com>
2023-12-04 17:29:32 +0800

    feat: add tdesign as UI library and updated lintstaged config

--- .lintstagedrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '*.{js,ts}': ['eslint --fix', 'prettier --write'],
-  '*.{css,less,scss,wxss}': ['stylelint --fix', 'prettier --write'],
-  '*.{js,ts,wxml,html,json,css,less,md}': ['prettier --write'],
-};
