4379a2012389ee30cd1ef3d66754e30f34ab9987
RoseBud13 <xiongkaijie13@gmail.com>
2023-12-05 18:08:26 +0800

    feat: add customized navbar and update lint config

---
+++ components/navbar/props.js
@@ -0,0 +1,36 @@
+const props = {
+  animation: {
+    type: Boolean,
+    value: true,
+  },
+  delta: {
+    type: Number,
+    value: 1,
+  },
+  externalClasses: {
+    type: Array,
+  },
+  fixed: {
+    type: Boolean,
+    value: true,
+  },
+  leftArrow: {
+    type: Boolean,
+    value: false,
+  },
+  title: {
+    type: String,
+  },
+  titleMaxLength: {
+    type: Number,
+  },
+  visible: {
+    type: Boolean,
+    value: true,
+  },
+  transparent: {
+    type: Boolean,
+    value: false,
+  },
+};
+export default props;
