cb59fa6f7e6a5a8be598d4b2c3417176f3cd664b
RoseBud13 <xiongkaijie13@gmail.com>
2023-12-06 17:59:12 +0800

    feat: add swiper effect

--- pages/brands/fotografiska/fotografiska.js
+++ pages/brands/fotografiska/fotografiska.js
@@ -1,6 +1,17 @@
 //Page Object
 Page({
-  data: {},
+  data: {
+    logo: 'https://www.fotografiska.com/favicon.ico',
+    homeswiper: {
+      indicatorDots: false,
+      vertical: true,
+      autoplay: false,
+      circular: true,
+      interval: 5000,
+      duration: 500,
+    },
+    homeMediaSrc: 'https://www.fotografiska.com/app/uploads/2022/09/Foto_Landing.mp4',
+  },
   //options(Object)
   onLoad: function () {},
   onReady: function () {},
